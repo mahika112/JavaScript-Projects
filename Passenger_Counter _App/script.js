6d652397e726476fd4e9f1d864d6575f7738bb1e
mahika112 <mahikasingh122333@gmail.com>
2025-07-26 10:59:14 +0530

    Initial commit - Lead Tracker Chrome Extension

---
+++ Passenger_Counter _App/script.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+
+
+let count=0
+function increment(){
+    count = count +1
+    countEl.innerText = count
+}
+
+function save(){
+   let countStr= count + " - "
+   saveEl.textContent += countStr
+   countEl.textContent=
+   count=0
+}
